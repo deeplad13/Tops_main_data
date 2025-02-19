9a5d0f0857b0c9e6a3cb7abfe0cbce0e53248237
deeplad13 <deeplad002@gmail.com>
2025-02-19 10:46:23 +0530

    Update working with database using sql.sql
    done

--- Assessment/Sql/working with database using sql.sql
+++ Assessment/Sql/working with database using sql.sql
@@ -25,6 +25,39 @@ action_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 select *from employee_audit;
 
+ -- TRIGGERS 
+ 
+ -- CREATE TRIGGER
+ Delimiter $$
+ create trigger after_employee_audit
+ after insert on employees
+ for each row
+ begin 
+ insert into employee_audit(employee_id,name,position,salary,hire_date)
+ values(new.Employee_id,new.name,new.position,new.salary,new.hire_date);
+ end;$$
+ Delimiter ;
+ 
+ -- CREATE STORED PROCEDURE
+ 
+ Delimiter $$
+ create procedure `add_employee` (
+ in emp_name varchar(100),
+ in emp_position varchar(100),
+ in emp_salary decimal(10,2),
+ in emp_hire_date date)
+ Begin
+ insert into employees(name, position, salary, hire_date) 
+ values(emp_name,emp_position,emp_salary,emp_hire_date);
+ end;$$
+ Delimiter ;
+ 
+ call add_employee("Rohit Verma","Data Analytics",95000.00,"2025-02-10");
+ call add_employee("Ajay Mehta","Web Developer",75000.00,"2024-12-12");
+ 
+ select *from employees;
+ select *from employee_audit;
+
 
 
 
